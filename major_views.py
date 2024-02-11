dcc0fa22c5cc26f64adc5e2489f9c7f68ab0bc8e
Darlan Sá <darlansaa@gmail.com>
2024-02-10 23:05:59 -0300

    ajustes lint

--- major_views.py
+++ major_views.py
@@ -22,11 +22,11 @@ def get_head():
     Returns:
         _type_: _description_
     """
-    head = {
+    head_auth = {
         "Authorization": "Bearer " + dotenv_values()["ACESS_TOKEN"],
         "Client-Id": dotenv_values()["CLIENT_ID"],
     }
-    return head
+    return head_auth
 
 
 head = get_head()
@@ -55,13 +55,13 @@ def insert_streamer(user_id, user_name, language):
     con.execute(query_insert)
 
 
-def analyser_stream(id):
+def analyser_stream(id_stream):
     """_summary_
 
     Returns:
         _type_: _description_
     """
-    return con.execute(f"SELECT id from stream where id = {id}").fetchone()
+    return con.execute(f"SELECT id from stream where id = {id_stream}").fetchone()
 
 
 def insert_stream(id_streamer, title, started_at, id):
@@ -95,7 +95,7 @@ def insert_stream_view(id_stream, viewer_count):
     Returns:
         _type_: _description_
     """
-    query_insert = f""" INSERT INTO major_copenhagen_2024.main.views_stream 
+    query_insert = f""" INSERT INTO major_copenhagen_2024.main.views_stream
             (id_stream, viewer_count)
             VALUES({id_stream}, '{viewer_count}')"""
     con.execute(query_insert)
@@ -141,7 +141,7 @@ else:
 
         title_stream = sub(r'[.,"\'-?:!;]', "", streamer["title"])
         started_at_stream = streamer["started_at"]
-        id_stream = streamer["id"]
+        id_stream_request = streamer["id"]
 
         viewer_count_stream = streamer["viewer_count"]
 
@@ -152,15 +152,17 @@ else:
                 user_name=user_name_streamer,
             )
 
-        if not analyser_stream(id=id_stream):
+        if not analyser_stream(id_stream=id_stream_request):
             insert_stream(
-                id=id_stream,
+                id_stream=id_stream_request,
                 id_streamer=user_id_streamer,
                 started_at=started_at_stream,
                 title=title_stream,
             )
 
         if not analyser_stream_view(
-            id_stream=id_stream, viewer_count=viewer_count_stream
+            id_stream=id_stream_request, viewer_count=viewer_count_stream
         ):
-            insert_stream_view(id_stream=id_stream, viewer_count=viewer_count_stream)
+            insert_stream_view(
+                id_stream=id_stream_request, viewer_count=viewer_count_stream
+            )
